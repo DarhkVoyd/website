43f7dbbfa5f47a6975ed0bd2b829bad916288a3f
D V <divyadityasnaruka@gmail.com>
2024-06-21 05:40:18 +0530

    refactor usePreferences to module

--- pages/tools/index.page.tsx
+++ pages/tools/index.page.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import fs from 'fs';
 import Head from 'next/head';
 import yaml from 'js-yaml';
@@ -9,11 +9,11 @@ import Sidebar from './sidebar/';
 import ToolingTable from './ToolingTable';
 import StyledMarkdown from '~/components/StyledMarkdown';
 import matter from 'gray-matter';
-import Fuse from 'fuse.js';
 import { DRAFT_ORDER } from '~/lib/config';
 import getUniqueValuesPerField, {
   UniqueValuesPerField,
 } from './getUniqueValuesPerField';
+import usePreferences from './usePreferences';
 
 export async function getStaticProps() {
   const toolingData = yaml.load(
@@ -167,134 +167,3 @@ export default function ToolingPage({
 }
 
 ToolingPage.getLayout = getLayout;
-
-function usePreferences(tools: Tooling[]) {
-  const [preferences, setPreferences] = useState<Preferences>({
-    query: '',
-    viewBy: 'toolingTypes',
-    sortBy: 'none',
-    languages: null,
-    licenses: null,
-    drafts: null,
-  });
-
-  const fuse = useMemo(() => {
-    return new Fuse(tools, {
-      keys: ['name'],
-      includeScore: true,
-      threshold: 0.3,
-    });
-  }, [tools]);
-
-  const hits = useMemo(() => {
-    if (preferences.query.trim() === '') {
-      return tools;
-    } else {
-      return fuse.search(preferences.query).map((result) => result.item);
-    }
-  }, [fuse, preferences.query, tools]);
-
-  const filteredHits = useMemo(() => {
-    if (
-      !preferences.languages &&
-      !preferences.licenses &&
-      !preferences.drafts
-    ) {
-      return hits;
-    }
-
-    return hits.filter((tool: Tooling) => {
-      if (preferences.languages && preferences.languages.length > 0) {
-        if (
-          !tool.languages ||
-          !preferences.languages.some((lang) =>
-            tool.languages.some((l) => l.toLowerCase() === lang.toLowerCase()),
-          )
-        ) {
-          return false;
-        }
-      }
-
-      if (preferences.licenses && preferences.licenses.length > 0) {
-        if (
-          !tool.license ||
-          !preferences.licenses.some(
-            (license) => license.toLowerCase() === tool.license.toLowerCase(),
-          )
-        ) {
-          return false;
-        }
-      }
-
-      if (preferences.drafts && preferences.drafts.length > 0) {
-        if (!tool.supportedDialects || !tool.supportedDialects.draft) {
-          return false;
-        }
-        const toolDrafts = tool.supportedDialects.draft.map(String);
-
-        if (!preferences.drafts.some((draft) => toolDrafts.includes(draft))) {
-          return false;
-        }
-      }
-
-      return true;
-    });
-  }, [hits, preferences.languages, preferences.licenses, preferences.drafts]);
-
-  const sortedHits = useMemo(() => {
-    if (preferences.sortBy === 'none') {
-      return filteredHits;
-    }
-
-    const compare = (a: Tooling, b: Tooling) => {
-      let aValue, bValue;
-
-      switch (preferences.sortBy) {
-        case 'name':
-          aValue = a.name.toLowerCase();
-          bValue = b.name.toLowerCase();
-          break;
-        case 'license':
-          aValue = a.license ? a.license.toLowerCase() : '';
-          bValue = b.license ? b.license.toLowerCase() : '';
-          break;
-        default:
-          return 0;
-      }
-
-      if (aValue < bValue) return -1;
-      if (aValue > bValue) return 1;
-      return 0;
-    };
-
-    return [...filteredHits].sort(compare);
-  }, [filteredHits, preferences.sortBy]);
-
-  const preferredData = useMemo(() => {
-    const data = {};
-
-    if (preferences.viewBy === 'all') {
-      data['all'] = sortedHits;
-    } else {
-      sortedHits.forEach((tool) => {
-        if (Array.isArray(tool[preferences.viewBy])) {
-          tool[preferences.viewBy].forEach((category) => {
-            if (!data[category]) {
-              data[category] = [];
-            }
-            data[category].push(tool);
-          });
-        }
-      });
-    }
-
-    return data;
-  }, [sortedHits, preferences.viewBy]);
-
-  return {
-    preferredData,
-    length: filteredHits.length,
-    preferences,
-    setPreferences,
-  };
-}
---
+++ pages/tools/usePreferences.tsx
@@ -0,0 +1,134 @@
+import Fuse from 'fuse.js';
+import { useMemo, useState } from 'react';
+import { Preferences, Tooling } from './index.page';
+
+export default function usePreferences(tools: Tooling[]) {
+  const [preferences, setPreferences] = useState<Preferences>({
+    query: '',
+    viewBy: 'all',
+    sortBy: 'name',
+    languages: null,
+    licenses: ['mit'],
+    drafts: ['2020-12'],
+  });
+
+  const fuse = useMemo(() => {
+    return new Fuse(tools, {
+      keys: ['name'],
+      includeScore: true,
+      threshold: 0.3,
+    });
+  }, [tools]);
+
+  const hits = useMemo(() => {
+    if (preferences.query.trim() === '') {
+      return tools;
+    } else {
+      return fuse.search(preferences.query).map((result) => result.item);
+    }
+  }, [fuse, preferences.query, tools]);
+
+  const filteredHits = useMemo(() => {
+    if (
+      !preferences.languages &&
+      !preferences.licenses &&
+      !preferences.drafts
+    ) {
+      return hits;
+    }
+
+    return hits.filter((tool: Tooling) => {
+      if (preferences.languages && preferences.languages.length > 0) {
+        if (
+          !tool.languages ||
+          !preferences.languages.some((lang) =>
+            tool.languages.some((l) => l.toLowerCase() === lang.toLowerCase()),
+          )
+        ) {
+          return false;
+        }
+      }
+
+      if (preferences.licenses && preferences.licenses.length > 0) {
+        if (
+          !tool.license ||
+          !preferences.licenses.some(
+            (license) => license.toLowerCase() === tool.license.toLowerCase(),
+          )
+        ) {
+          return false;
+        }
+      }
+
+      if (preferences.drafts && preferences.drafts.length > 0) {
+        if (!tool.supportedDialects || !tool.supportedDialects.draft) {
+          return false;
+        }
+        const toolDrafts = tool.supportedDialects.draft.map(String);
+
+        if (!preferences.drafts.some((draft) => toolDrafts.includes(draft))) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+  }, [hits, preferences.languages, preferences.licenses, preferences.drafts]);
+
+  const sortedHits = useMemo(() => {
+    if (preferences.sortBy === 'none') {
+      return filteredHits;
+    }
+
+    const compare = (a: Tooling, b: Tooling) => {
+      let aValue, bValue;
+
+      switch (preferences.sortBy) {
+        case 'name':
+          aValue = a.name.toLowerCase();
+          bValue = b.name.toLowerCase();
+          break;
+        case 'license':
+          aValue = a.license ? a.license.toLowerCase() : '';
+          bValue = b.license ? b.license.toLowerCase() : '';
+          break;
+        default:
+          return 0;
+      }
+
+      if (aValue < bValue) return -1;
+      if (aValue > bValue) return 1;
+      return 0;
+    };
+
+    return [...filteredHits].sort(compare);
+  }, [filteredHits, preferences.sortBy]);
+
+  const preferredData = useMemo(() => {
+    const data = {};
+
+    if (preferences.viewBy === 'all') {
+      data['all'] = sortedHits;
+    } else {
+      sortedHits.forEach((tool) => {
+        if (Array.isArray(tool[preferences.viewBy])) {
+          tool[preferences.viewBy].forEach((category) => {
+            if (!data[category]) {
+              data[category] = [];
+            }
+            data[category].push(tool);
+          });
+        }
+      });
+    }
+
+    return data;
+  }, [sortedHits, preferences.viewBy]);
+
+  return {
+    preferredData,
+    length: filteredHits.length,
+    preferences,
+    setPreferences,
+  };
+}
